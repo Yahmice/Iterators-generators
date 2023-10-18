51108f34235f1db7b046e802362efaa8336b04c8
Yahmice <83534639+Yahmice@users.noreply.github.com>
2023-10-18 12:08:22 +0500

    Update and rename main.py to 1-st task.py

---
+++ 1-st task.py
@@ -0,0 +1,43 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+
+    def __iter__(self):
+        self.index1 = 0
+        self.index2 = 0
+        return self
+
+    def __next__(self):
+        if self.index1 == len(self.list_of_list):
+            raise StopIteration
+        item1 = self.list_of_list[self.index1]
+        item2 = item1[self.index2]
+        self.index2 += 1
+        if self.index2 == len(item1):
+            self.index2 = 0
+            self.index1 += 1
+        return item2
+
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
